f1ac36d00f1c4a02e9de14ab6f2bcb4cdf1a551a
glgibran7 <gibrangl1167@gmail.com>
2025-09-27 19:53:14 +0800

    pertama

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello, World!</Text>
+    </View>
+  );
+};
+
+export default App;
